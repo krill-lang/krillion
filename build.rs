66f21bb2bb4452b7f976e1d30f28bbee0d030bf7
funnsam <funnsam8@gmail.com>
2024-02-09 06:43:56 +0000

    fix awkward word

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 
 fn main() {
     let mut commit = Command::new("git")
-        .args(&["rev-parse", "HEAD"])
+        .args(["rev-parse", "HEAD"])
         .output()
         .ok()
         .and_then(|out| String::from_utf8(out.stdout).ok())
@@ -10,11 +10,11 @@ fn main() {
         .unwrap_or(r"¯\_(ツ)_/¯".to_string());
 
     if matches!(Command::new("git")
-        .args(&["diff-index", "--quiet", "HEAD"])
+        .args(["diff-index", "--quiet", "HEAD"])
         .status()
         .ok()
         .map(|a| !a.success()), Some(true)) {
-        commit += " (changed)"
+        commit += " (modified)"
     }
 
     println!("cargo:rustc-env=COMMIT={commit}");
